3991e21fc30c3713594b18b8bcfb3d4bc6c3c39e
dannick stark <dannickstark@gmail.com>
2021-01-09 05:06:52 +0100

    dump

---
+++ lib/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+      <Router-view></Router-view>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang = "scss">
+  html,body{    
+    overflow: hidden !important;
+    overflow-x: hidden !important;
+    overflow-y: hidden !important;
+  }
+</style>
